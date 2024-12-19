c9e715d69c7d7d081188267d171c1d73d0d8534f
Mariano Garmendia <marianogarmendia77@gmail.com>
2024-12-18 23:53:38 -0300

    first iteration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log(Bun.env)
+import express from "express";
+
+const PORT = process.env.port || 3000;
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`server started at port: ${PORT}`);
+});
